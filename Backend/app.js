542caf566c8b8c39a2d06b766f551e4ec47b13dd
Ivan <janivantot@gmail.com>
2025-03-23 21:16:24 +0800

    added course,section,student,subject models

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,18 @@ import dotenv from "dotenv";
 import cors from "cors";
 import createDatabaseIfNotExists from "./Services/databaseCreate.js";
 import db from "./database.js";
+import {
+  createTableUserAccounts,
+  createTableAdminAccounts,
+  createTableSectionTable,
+  createTableSubjectTable,
+  createTableCourseTable,
+} from "./Services/tableCreate.js";
+import {
+  insertSubjectIfNotExist,
+  insertSectionIfNotExist,
+  insertCourseIfNotExist,
+} from "./Services/valueInserter.js";
 
 dotenv.config();
 const app = express();
@@ -17,7 +29,17 @@ async function initializeApp() {
     await createDatabaseIfNotExists();
     await db.authenticate();
 
+    await createTableSectionTable();
+    await createTableSubjectTable();
+    await createTableCourseTable();
+    await createTableUserAccounts();
+    await createTableAdminAccounts();
+
     console.log("Tables have been created or checked.");
+
+    await insertSubjectIfNotExist();
+    await insertSectionIfNotExist();
+    await insertCourseIfNotExist();
 
     const server = app.listen(process.env.PORT, () => {
       console.log(`App is listening on port: ${process.env.PORT}`);
