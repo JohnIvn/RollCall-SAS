02e8e53ab541d14a9a5a3d243734da91bfd1f50d
Ivan <janivantot@gmail.com>
2025-03-24 20:55:23 +0800

    not allow consecutive swipes

--- Backend/Controllers/testController.js
+++ Backend/Controllers/testController.js
@@ -2,9 +2,20 @@ import Test from "../Models/testModel.js";
 import Banned from "../Models/bannedModel.js";
 import { studentAccount } from "../Models/studentAccountModel.js";
 
+let lastScannedCard = null; 
+
 export const saveTestData = async (data) => {
   try {
     const cleanedData = data.replace(/^Card Scanned:\s*/, "");
+
+    if (cleanedData === lastScannedCard) {
+      console.log(
+        `Consecutive duplicate scan detected for ${cleanedData}. Ignoring.`
+      );
+      return null;
+    }
+
+    lastScannedCard = cleanedData;
 
     const bannedEntry = await Banned.findOne({
       where: { Banned_hex: cleanedData },
@@ -14,6 +25,7 @@ export const saveTestData = async (data) => {
       console.log(`Banned card detected: ${cleanedData}. Data not logged.`);
       return null;
     }
+
     const student = await studentAccount.findOne({
       where: { cardNumber: cleanedData },
     });
