1eefedb3d87c6e604d09c9b319b322f1ec792d99
Matthew Cania <matthewgab24@gmail.com>
2025-03-24 22:49:21 +0800

    couldnt finish Admin Panel, will finish it tomorrow along with testing the backend

--- Frontend/src/pages/Admin.jsx
+++ Frontend/src/pages/Admin.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown, faCheckCircle, faSearch } from '@fortawesome/free-solid-svg-icons'
 import UccLogo from '/ucc.png'
 
 
@@ -12,30 +12,30 @@ export default function AdminPage(){
 			<div
 				className="flex justify-between items-center w-full gap-10 p-2"
 				style={{ minHeight: "100%", height: 'auto' }}
-			>|
+			>
 				<div
 					className="flex flex-col w-2/5 h-full bg-white rounded-2xl"
 				>
 
 					<div
-						className='flex justify-center items-center w-full h-1/4 m-4'
+						className='flex justify-center items-center w-auto h-1/4 m-4'
 					>
 
 						<div
-							className='flex rounded-full h-32 w-32 border-2 border-emerald-800 overflow-hidden'
+							className='flex rounded-full h-26 w-26 border-2 border-emerald-800 overflow-hidden'
 						>
 							<img src={UccLogo} alt="" />
 						</div>
 						<div
-							className='flex flex-col items-start px-2 justify-center h-full w-2/3'
+							className='flex flex-col items-center px-2 justify-center h-full w-2/3'
 						>
 							<h1
-								className='text-md font-semibold'
+								className='w-full text-start text-md font-semibold'
 							>
 								Matthew Gabriel M. Cania
 								</h1>
 							<p
-								className='text-sm'
+								className='text-sm w-full text-start'
 							>
 								Database Management Professor
 							</p>
@@ -46,34 +46,142 @@ export default function AdminPage(){
 					<div
 						className='flex flex-col h-full w-full justify-start items-center'
 					>
+					<h1
+						className='flex w-8/9 justify-start items-center font-semibold'
+					>
+						Registered Subject:
+					</h1>
 						<div
-							className='flex justify-between items-centerbg-blue-950 mx-4 w-9/10 h-18 rounded-2xl'
+							className='flex justify-between items-center bg-blue-950 mx-4 w-9/10 h-16 rounded-2xl m-2'
 						>
 							<h1
-								className='flex justify-start items-center text-xl mx-2'
+								className='flex justify-start items-center text-xl mx-2 text-white'
 							>
 								Database Management Systems
 							</h1>
 							<FontAwesomeIcon
-								className='flex'
+								className='flex w-10 text-white'
 							icon={faCaretDown}/>
 						</div>
+
+						<h1
+							className='flex w-8/9 justify-start items-center font-semibold'
+						>
+							Schedule:
+						</h1>
+
+						<div
+							className='flex justify-between items-center bg-blue-300 mx-4 w-9/10 h-14 rounded-2xl m-2'
+						>
+							<FontAwesomeIcon
+								className='flex w-16 text-2xl text-black'
+								icon={faCheckCircle}
+							/>
+							<h1
+								className='flex justify-start items-center w-full text-md mx-2 text-black'
+							>
+								Mon -  10:00AM to 12:00PM
+							</h1>
+						</div>
+						
+						<div
+							className='flex justify-between items-center bg-blue-300 mx-4 w-9/10 h-14 rounded-2xl m-2'
+						>
+							<FontAwesomeIcon
+								className='flex w-16 text-2xl text-black'
+								icon={faCheckCircle}
+							/>
+							<h1
+								className='flex justify-start items-center w-full text-md mx-2 text-black'
+							>
+								Mon -  10:00AM to 12:00PM
+							</h1>
+						</div>
+
+						
+						<div
+							className='flex justify-between items-center bg-blue-300 mx-4 w-9/10 h-14 rounded-2xl m-2'
+						>
+							<FontAwesomeIcon
+								className='flex w-16 text-2xl text-black'
+								icon={faCheckCircle}
+							/>
+							<h1
+								className='flex justify-start items-center w-full text-md mx-2 text-black'
+							>
+								Mon -  10:00AM to 12:00PM
+							</h1>
+						</div>
+						
+						<div
+							className='flex justify-between items-center bg-blue-300 mx-4 w-9/10 h-14 rounded-2xl m-2'
+						>
+							<FontAwesomeIcon
+								className='flex w-16 text-2xl text-black'
+								icon={faCheckCircle}
+							/>
+							<h1
+								className='flex justify-start items-center w-full text-md mx-2 text-black'
+							>
+								Mon -  10:00AM to 12:00PM
+							</h1>
+						</div>
+						
+						<div
+							className='flex justify-between items-center bg-blue-300 mx-4 w-9/10 h-14 rounded-2xl m-2'
+						>
+							<FontAwesomeIcon
+								className='flex w-16 text-2xl text-black'
+								icon={faCheckCircle}
+							/>
+							<h1
+								className='flex justify-start items-center w-full text-md mx-2 text-black'
+							>
+								Mon -  10:00AM to 12:00PM
+							</h1>
+						</div>
+						
 					</div>
 
 				</div>
+
+
 				<div
-					className="flex flex-col w-3/5 h-full"
+					className="flex flex-col justify-start items-center w-3/5 h-full rounded-2xl overflow-hidde"
 				>
 
-					<div>
-						<div>
-							<h1>Enrolled Students</h1>
+					<div
+						className='flex items-center justify-center w-full h-1/5 p-4'
+					>
+						<div
+							className='flex flex-col justify-center items-start w-3/5 h-full'
+						>
+							<h1
+								className='flex w-full h-auto text-white text-xl'
+							>
+								Enrolled Students
+							</h1>
+							<p
+								className='flex w-full h-auto text-white text-xl'
+							>
+								Bachelors In Information and Technology
+							</p>
+						</div>
+						<div
+							className='flex justify-center items-center w-2/5 h-full relative'
+						>
+							<input 
+								className='flex h-10 w-full rounded-xl border border-gray-600 text-white placeholder-gray-400 pl-8'
+								placeholder='Search...'
+								type="text" 
+							/>
+							<FontAwesomeIcon
+								className='flex justify-center items-center text-gray-400 absolute left-0 px-2'
+								icon={faSearch}/>
 						</div>
 					</div>
-					<div>
 
-
-					</div>
+					
 
 				</div>
 
